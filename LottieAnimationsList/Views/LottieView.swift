98e393d50fb7cd1f3118a97b95ee5734d8b0bc78
lcs-nhwang <sjnhwang@lcs.on.ca>
2025-02-10 13:54:56 -0500

    Added Mr. Gordon's helper view.

---
+++ LottieAnimationsList/Views/LottieView.swift
@@ -0,0 +1,18 @@
+//
+//  LottieView.swift
+//  LottieAnimationsList
+//
+//  Created by Nicholas Hwang on 10/2/2025.
+//
+
+import SwiftUI
+
+struct LottieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LottieView()
+}
